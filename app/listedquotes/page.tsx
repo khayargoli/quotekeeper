7eaab4fefba1d7c0ac7520e0bb8679da207fe394
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2024-08-28 20:27:14 +0545

    applied caching on listedquotes

--- app/listedquotes/page.tsx
+++ app/listedquotes/page.tsx
@@ -3,6 +3,7 @@ import prisma from '../db/db';
 import { klee_One, sacramento } from '../fonts';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
+export const revalidate = 0; // Disables caching for this component, fetches on every request.
 
 const ListedQuotes = async () => {
 
